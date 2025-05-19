31a87f7fd2e7a3237946019e718472a7e053a66b
Андрей <adlerSt@yandex.ru>
2025-05-19 21:45:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int age = 36;
+        String name = "Andrey";
+
+        double numberOne = 17.6;
+        double numberTwo = 15.2;
+
+        System.out.println("My name is" + name + ". My age is " + age + " years old.");
+
+        System.out.println("The sum of two numbers = " + numberOne + numberTwo);
+    }
+}
