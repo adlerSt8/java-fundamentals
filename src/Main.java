9c1ade3a7ea012f021efbf212bbd8a2553d5049e
Андрей <adlerSt@yandex.ru>
2025-05-21 18:23:43 +0300

    feat: add age check program and password

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,5 +12,25 @@ public class Main {
 
         System.out.println("My name is" + name + ". My age is " + age + " years old.");
         System.out.println("The sum of two numbers = " + numberOne + numberTwo);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String nameUser = scanner.nextLine();
+        System.out.println("Введите возраст: ");
+        int ageUser = scanner.nextInt();
+        System.out.println("Тебя зовут " + name + " и ты ");
+        if (ageUser > 0 && ageUser < 18) {
+            System.out.print(" подросток.");
+        } else if (ageUser >= 18 && ageUser < 60) {
+            System.out.println(" взрослый.");
+        } else if (ageUser >= 60 && ageUser < 125){
+            System.out.println(" пожилой.");
+        } else System.out.println(" не существуешь!");
+
+        System.out.println("Введите пароль: ");
+        String password = scanner.nextLine();
+        if (password.equals("admin123")) {
+            System.out.println("Доступ разрешен!");
+        } else System.out.println("Ошибка доступа!");
     }
 }
